af3d6cc4418e6600cf093d1f3531417d86865049
Michael <michaelnz10@outlook.com>
2022-11-22 19:58:03 +1300

    Resize window for Mac

--- src/main.js
+++ src/main.js
@@ -7,14 +7,19 @@ import App from "./App.vue";
 import {appWindow, LogicalSize} from "@tauri-apps/api/window";
 
 
+
 const app = createApp(App);
 
 app.use(ElementPlus);
 app.mount("#app");
 
-const platformName = await platform();
+const resizeWindowForMac = async () => {
+    const platformName = await platform();
 
-if (platformName === 'darwin') {
-    await appWindow.setSize(new LogicalSize(800, 660))
+    if (platformName === 'darwin') {
+        await appWindow.setSize(new LogicalSize(800, 660))
+    }
 }
+
+resizeWindowForMac();
 
